a8522e313d162041b0356f57f63f7d08e4614bc9
Squibst <phd.tokarchuk@icloud.com>
2025-06-16 15:46:59 -0500

    merge

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function john() {
-  document.getElementById("john").play();
+  var audio = document.getElementById("john");
+  audio.currentTime = 0;
+  audio.play();
 }
 function clickSound(){
-  document.getElementById("click").play();
+  var audio = document.getElementById("click");
+  audio.currentTime = 0;
+  audio.play();
+}
+function tickSound(){
+  var audio = document.getElementById("tick");
+  audio.currentTime = 0;
+  audio.play();
 }
