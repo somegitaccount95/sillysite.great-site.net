3d84eae544b97a5d3597ecc19afbc8de0099d77d
Squibst <phd.tokarchuk@icloud.com>
2025-06-16 17:58:17 -0500

    epic changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let porkAudio = document.getElementById("john");
+let tickAudio = document.getElementById("tick");
+let clickAudio = document.getElementById("click");
+let dark =  true;
 
 function john() {
   porkAudio.currentTime = 0;
@@ -7,9 +10,34 @@ function john() {
 }
 
 function clickSound(){
-  document.getElementById("click").play();
+  clickAudio.currentTime = 0;
+  clickAudio.pause();
+  clickAudio.play();
+}
+
+function tickSound(){
+  tickAudio.currentTime = 0;
+  tickAudio.pause();
+  tickAudio.play();
 }
 
 function shrekSpin() {
   document.getElementById("shrek").classList.toggle("spinning");
+}
+
+function toggle() {
+  if(!dark){
+    document.getElementById("dark-mode").innerHTML = "dark mode";
+  }else{
+    document.getElementById("dark-mode").innerHTML = "light mode";
+  }
+  document.getElementById("dark-mode").classList.toggle("light-button");
+
+  document.getElementsByTagName("body")[0].classList.toggle("dark");
+  document.getElementById("welcome-bar").classList.toggle("dark-bar");
+
+  document.querySelectorAll("button").forEach(element => {
+    element.classList.toggle("light-shadow");
+  });
+  dark = !dark;
 }
