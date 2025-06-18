a64fd6dcca06ab7d8d63177c2a8e3e0abc1695ea
Squibst <phd.tokarchuk@icloud.com>
2025-06-17 19:34:29 -0500

    new nav bar

--- javascript/elements.js
+++ javascript/elements.js
@@ -3,10 +3,31 @@ class TopHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = 
     `
-    <div id="welcome-bar">
-      <h2 id="welcome"><a href="/">WELCOME TO NON SILLY SITE</a></h2>
-      <img onclick="john()" id="pork" src="images/john-pork.jpg">
-    </div>
+    <header>
+            <a href="/">
+                <h1 id="logo">
+                    NON SILLY WEBSITE
+                </h1>
+            </a>
+            <div id="nav-buttons">
+                <a href="https://kug.kesug.com" target="_blank">
+                    <button onclick="clickSound()" onmouseover="tickSound()" class="button nav-button color-button" id="red-button">
+                        riba
+                    </button>
+                </a>
+                <a href="about.html">
+                    <button onclick="clickSound()" onmouseover="tickSound()" class="button nav-button color-button" id="blue-button">
+                        about
+                    </button>
+                </a>
+                <a href="contact.html">
+                    <button onclick="clickSound()" onmouseover="tickSound()" class="button nav-button color-button" id="green-button">
+                        contact
+                    </button>
+                </a>
+            </div>
+            <img onclick="john()" id="pork" src="images/john-pork.jpg">
+        </header>
     `
   }
 }
